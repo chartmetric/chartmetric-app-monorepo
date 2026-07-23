14473076ae98dc74c71f42698716da8b9571401e
Itai Levi <itai@chartmetric.com>
2026-07-23 15:16:15 -0400

    fix: add new app + fix start host for web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '0.0.0.0',
+      'new-app.chartmetric.com'
+    ]
+  }
 });
